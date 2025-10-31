5659f127bc6e92a1ded7064f427db81099f727c3
Timothy Dodd <tdodd@me.com>
2025-10-31 10:59:53 -0400

    feat: update server configuration and improve request handling in server.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes.js';
+import { routes } from './app.routes';
 export function tokenGetter() {
   return localStorage.getItem('authToken');
 }
